d95741bb3504fb96a4030ea5694a6a1118163445
Allan Brunner <allan@allanbrunner.dev>
2025-07-29 11:39:13 +0200

    Introduces CSV writing functionality
    Implements a separate module for writing data to CSV files,
    improving code organization and reusability.
    
    The main function now utilizes this new module to export
    query results to a CSV file, streamlining the data
    export process.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod db;
+mod file;
 
-use csv::WriterBuilder;                     // Allow to export data to CSV
 use dotenv::dotenv;                         // Loads environment variables from a `.env` file at startup.
 use anyhow::Error;                          // A convenient error type that can represent any error (`?` will convert).
-use std::{                                  // Import standard-library items:
-    env,                                    //      Handles environment variables and arguments
-    fs::File                                //      Provide access to open files
-};
+use std::env;                               // Handles environment variables and arguments
+
 use db::SqlClient;
+use file::write_csv_record;
 
 /// Maximum number of rows fetched per batch. Fetching in batches is more efficient than row-by-row.
 const BATCH_SIZE: usize = 200;
@@ -23,24 +22,14 @@ fn main() -> Result<(), Error> {
     let user    = env::var("DB_USER").expect("DB_USER must be set");
     let pwd    = env::var("DB_PASS").expect("DB_PASS must be set");
 
-    // Prepare the .csv output file
-    let file = File::create("output.csv")?;
-    let mut writer = WriterBuilder::new()
-        .delimiter(b';').from_writer(file);
-
     // Initialize the SQL client
     let client = SqlClient::new(&server, &port, &name, &user, &pwd, BATCH_SIZE)?;
 
     // Retrieve the result of the query with the header
     let result = client.execute_query("SELECT TOP 15 * FROM FCF_NOTIFICATION", true)?;
 
-    // For each row in result, write it in the csv file
-    for row in result {
-        writer.write_record(row)?;
-    }
-
-    // Flush the content in the file
-    writer.flush()?;
+    // Write result in CSV
+    write_csv_record("output.csv", result)?;
     println!("Done! Results written to output.csv");
 
     // Return Ok if no error occured
